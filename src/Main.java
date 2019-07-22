e3cd4db27dfb6e51c639d6188b1964b5a443f588
milan <milan.kvd1991@gmail.com>
2019-07-22 13:59:01 +0100

    'sumprogramadded'

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello World!");
+        int a,b,c;
+        a=5;
+        b=10;
+        c=a+b;
+        System.out.println(sum);
     }
 }
